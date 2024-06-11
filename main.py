1672c50f7e497920aa37999c5fd09d77ecca7eed
BramLankhorst <b.k.lankhorst@students.uu.nl>
2024-06-11 18:27:54 +0200

    example concatination

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import numpy as np
 from data_preprocessing import preprocess
 
-X_train, y_train, X_dev, y_dev, X_test, y_test = preprocess()
+X_train, X_p_train, y_train,X_dev ,X_p_dev, y_dev, X_test,X_p_test, y_test = preprocess()
+
+# concatenate protected attribute
+#print(X_p_train)
+X_p_train = X_p_train.reshape(-1,1)
+X_train_with_p = np.concatenate((X_train,X_p_train), axis=1)
+#print(X_train_with_p[:,9])
