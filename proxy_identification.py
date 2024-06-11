2896722721776692821188cdeafa9d809127e6b3
teuncb <50371179+teuncb@users.noreply.github.com>
2024-06-11 15:14:37 +0200

    Added requirements
    Can be used with "pip install -r requirements.txt"

--- proxy_identification.py
+++ proxy_identification.py
@@ -3,15 +3,23 @@ import pandas as pd
 #from sklearn import
 
 from data_preprocessing import preprocess
+from feature_imp import GAM, logreg
 
-def model(X, y):
+def model(X_train, y_train, X_dev, y_dev, X_test, y_test, model_type="logreg"):
     """Return a model object for determining y based on X"""
-    # TODO
-    pass
+    match model_type:
+        case 'GAM':
+            trained_model = GAM(X_train, y_train, X_dev, y_dev, X_test, y_test)
+        case 'logreg':
+            trained_model = logreg(X_train, y_train, X_dev, y_dev, X_test, y_test)
+        case _:
+            raise Exception(f'Model type "{model_type}" not recognised.')
+
+    return trained_model
 
 def get_feature_importance(model) -> np.array:
     """Returns an array of values for feature importance. Does not include the protected attribute (if it was in X)"""
-    # TODO
+    # TODO update after SHAP implemented
     pass
 
 def palabuhu_values(importance_with_xp, importance_without_xp, importance_predicting_xp) -> np.array:
@@ -20,11 +28,11 @@ def palabuhu_values(importance_with_xp, importance_without_xp, importance_predic
     # TODO Arbitrary placeholder, we need to check how these features look to decide on this operation
     return abs(importance_with_xp - importance_without_xp) * importance_predicting_xp
 
-if __name__ == '__main__':
-    X_train, y_train, X_test, y_test = preprocess()
+if __name__ == '__main__': # TODO update after vector with protected attribute returned from preprocessing
+    X_train, y_train, X_dev, y_dev, X_test, y_test = preprocess()
     protected_attribute = 'sex' # TODO placeholder
 
-    model_with_xp = model(X_train, y_train)
+    model_with_xp = model(X_train, y_train, model_type='GAM')
     importance_with_xp = get_feature_importance(model_with_xp)
 
     # Slice xp out of the features
